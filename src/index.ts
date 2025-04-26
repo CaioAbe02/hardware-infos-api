a1e3282d825a6c4037b5f90d6c596449961512b0
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-04-26 08:54:38 -0300

    feat: mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { App } from "./app"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
-new App().server.listen(3000)
+dotenv.config()
+
+const app = new App()
+const port = process.env.PORT || 5000
+const mongodb_url = process.env.MONGODB_URL || ""
+
+mongoose.connect(mongodb_url)
+app.server.listen(port)
