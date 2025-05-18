a55d292413854859587d4da4ce62575a32d7b921
Caio Abe <caioabe68@gmail.com>
2025-05-17 23:04:56 -0300

    feat: login and user routes

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,10 +1,14 @@
 import { Application } from "express"
 import { TestRoutes } from "./TestRoutes"
 import { GpuRoutes } from "./GpuRoutes"
+import { LoginRoutes } from "./LoginRoutes"
+import { UserRoutes } from "./UserRoutes"
 
 export default class Routes {
   constructor(server: Application) {
     server.use("/api/v1/test", TestRoutes)
     server.use("/api/v1/gpus", GpuRoutes)
+    server.use("/api/v1/login", LoginRoutes)
+    server.use("/api/v1/users", UserRoutes)
   }
 }
